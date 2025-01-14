7839df14c2cf694d2f43a8d7a84f2b6ae1ccffee
Sanjay <np03cs4a230207@heraldcollege.edu.np>
2025-01-14 07:39:39 +0545

    Getter Setter Updated

--- src/Workshop8/Student.java
+++ src/Workshop8/Student.java
@@ -1,10 +1,23 @@
 package Workshop8;
 
 public class Student {
-	String name;
-	int age;
-	
-	Student(){
-		System.out.println("This is Student class constructor.");
-	}
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
